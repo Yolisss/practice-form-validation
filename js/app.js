1cf086ff0082641e18915c3870e7c6bbb1e68b8b
Yolis <yolisisabel28@gmail.com>
2023-11-02 08:28:59 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,43 @@
+const form = document.querySelector('#form');
+const usernameInput = document.querySelector('#username');
+const emailInput = document.querySelector('#email');
+const passwordInput = document.querySelector('#password');
+
+// Usernames can only contain letters a-z in lowercase
+const isValidUsername = () => /^[a-z]+$/.test(usernameInput.value);
+
+// Password must contain a lowercase, uppercase letter and a number
+const isValidPassword = () => /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*$/.test(passwordInput.value);
+
+// Email must contain an @ symbol and a domain name
+const isValidEmail = () => /^[^@]+@[^@.]+\.[a-z]+$/i.test(emailInput.value);
+
+
+// Add an event listener to the form listening for the submit event 
+  form.addEventListener('submit', (e) =>{
+    console.log(e, 'my event obj');
+    isValidUsername();
+    //if username is all lowercase
+    if(isValidUsername()){
+      usernameInput.parentElement.classList.add('valid')
+    } else{
+      e.preventDefault();
+      usernameInput.classList.add('error')
+      document.querySelector('span').hidden = false; 
+    }
+  })
+  // validate the username by calling the isValidUsername function
+  // if the username is valid, 
+    // add the class "valid" to the parent label of the input
+  // else 
+    // prevent the form from being submitted
+    // set the class of the label to be "error"
+    // change the display value of the span to show the hint to the user
+
+
+  // validate the email by calling the isValidEmail function
+    // apply the error or valid class based on the returned value
+
+
+  // validate the password by calling the isValidPassword function
+    // apply the error or valid class based on the returned value
